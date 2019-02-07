3f258f7637441daceaca62588d5e1b04f3deb277
pauljd120 <46468834+pauljd120@users.noreply.github.com>
2019-02-06 19:01:35 -0500

    prompt user for file name and validate file name

--- exec_dash.py
+++ exec_dash.py
@@ -1,1 +1,16 @@
 #exec_dash.py
+
+import pandas as pd
+import os
+
+print("Input file name: ")
+
+input_file = input()
+
+print("/Users/pauldougherty/Desktop/python/p2/" + input_file)
+
+if (os.path.isfile("/Users/pauldougherty/Desktop/python/p2/data/" + input_file)):
+    print("file found!")
+else:
+    print("file not found :(")
+

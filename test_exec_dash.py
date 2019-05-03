2b269751a24829812709c3b2c43cb64c2989ca40
pauljd120 <46468834+pauljd120@users.noreply.github.com>
2019-05-03 17:58:01 -0400

    Update test_exec_dash.py

--- test_exec_dash.py
+++ test_exec_dash.py
@@ -12,19 +12,19 @@ def test_to_usd():
 
 def test_get_top_sellers():
     file_name = "201711.csv"
-    
+
     file_path = os.path.join("data/", file_name)
-    
+
     file_stats = pd.read_csv(file_path)
-    
+
     month = int(file_name[5:6])
     year = file_name[:4]
-    
+
     product_and_sales = []
-    
+
     result = get_top_sellers(file_stats, product_and_sales)
-    
-    assert result == "[{'product': 'Button-Down Shirt', 'sales': 5399.15}, {'product': 'Super Soft Hoodie', 'sales': 1950.0}, {'product': 'Khaki Pants', 'sales': 1869.0}, {'product': 'Super Soft Sweater', 'sales': 1349.91}, {'product': 'Vintage Logo Tee', 'sales': 669.9}, {'product': 'Sticker Pack', 'sales': 265.5}, {'product': 'Brown Boots', 'sales': 250.0}, {'product': 'Winter Hat', 'sales': 207.19999999999996}]"
+
+    assert str(result[0]) == "{'product': 'Button-Down Shirt', 'sales': 5399.15}"
     
     
     

52674fdf5913f2906009603d46710d4c7a28b524
pauljd120 <46468834+pauljd120@users.noreply.github.com>
2019-05-03 17:28:02 -0400

    Update test_exec_dash.py

--- test_exec_dash.py
+++ test_exec_dash.py
@@ -1,8 +1,31 @@
 from exec_dash import to_usd, get_top_sellers
 
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+import operator
+import matplotlib.ticker as ticker
+
 def test_to_usd():
     result = to_usd(50)
     assert result == "$50.00"
 
 def test_get_top_sellers():
+    input_file = "201711.csv"
+    
+    file_path = os.path.join("data/", input_file)
+    
+    file_stats = pd.read_csv(file_path)
+    
+    month = int(input_file[5:6])
+    year = input_file[:4]
+    
+    product_and_sales = []
+    
+    result = get_top_sellers(file_stats, product_and_sales)
+    
+    
+    
+    
+    
     
